4acc915f56bd19fa7bab00a4ac64ee83f7becb1b
marksman2op <56642081+marksman2op@users.noreply.github.com>
2020-06-29 21:17:35 +0530

    Create LCA.cpp

---
+++ LCA.cpp
@@ -0,0 +1,40 @@
+// LCA using Binary Lifting
+// Distance between 2 nodes = lvl[u] + lvl[v] - 2 * lvl[lca(u, v)]
+int l;
+vector<vector<int>> anc;     // anc[i][j] is the 2^j-th ancestor of the node i 
+
+void dfs_lca(int v, int p) {
+    in[v] = timer++;
+    anc[v][0] = p;
+    for (int i = 1; i <= l; i++)
+        anc[v][i] = anc[anc[v][i - 1]][i - 1];
+
+    for (int u : adj[v]) {
+        if (u != p)
+            dfs_lca(u, v);
+    }
+
+    out[v] = timer++;
+}
+
+bool is_ancestor(int u, int v) {
+    return (in[u] <= in[v] && out[u] >= out[v]);
+}
+
+int lca(int u, int v) {
+    if (is_ancestor(u, v))
+        return u;
+    if (is_ancestor(v, u))
+        return v;
+    for (int i = l; i >= 0; i--) {
+        if (!is_ancestor(anc[u][i], v))
+            u = anc[u][i];
+    }
+    return anc[u][0];
+}
+
+void pre(int r) {
+    l = ceil(log2(n));
+    anc.assign(n + 1, vector<int>(l + 1));   
+    dfs_lca(r, r);
+}
