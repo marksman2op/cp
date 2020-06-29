77c25204ea951665aa66ced7bf2fdb8e8da5516d
marksman2op <56642081+marksman2op@users.noreply.github.com>
2020-06-29 20:58:45 +0530

    Update nCr in O(n).cpp

--- nCr in O(n).cpp
+++ nCr in O(n).cpp
@@ -1,16 +1,17 @@
 //nCr in O(n)
 const int mod = 1e9 + 7;
 const int  N = 1e6 + 5;
+
 int fac[N], inv[N];
 
 void pre() { 
     fac[0] = inv[0] = inv[1] = 1;
+    for(int i = 1; i < N; i++) 
+        fac[i] = i * fac[i - 1] % mod;
     for(int i = 2; i < N; i++) 
         inv[i] = inv[mod % i] * (mod - mod / i) % mod;
     for(int i = 1; i < N; i++) 
         inv[i]= inv[i] * inv[i - 1] % mod;
-    for(int i = 1; i < N; i++) 
-        fac[i] = i * fac[i - 1] % mod;
 }
 
 int ncr(int n, int r) {
