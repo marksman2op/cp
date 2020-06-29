42258f479a13db7d8ec5048a43dc8eb36d84c830
marksman2op <56642081+marksman2op@users.noreply.github.com>
2020-06-30 01:31:14 +0530

    Update and rename cntInversionSegTree.cpp to inversionCntSegTree.cpp

--- inversionCntSegTree.cpp
+++ inversionCntSegTree.cpp
@@ -64,9 +64,9 @@ signed main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     
     int T = 1; 
-    cin >> T; 
+    // cin >> T; 
     while (T--) {
-        cin >> ws >> n;
+        cin >> n;
         for(int i = 0; i < n; i++)
             cin >> a[i];
 
