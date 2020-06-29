31d69a7aefcd2d83522461e0e1fdb31ceb413dbf
marksman2op <56642081+marksman2op@users.noreply.github.com>
2020-06-29 20:54:59 +0530

    Add files via upload

---
+++ DSU.cpp
@@ -0,0 +1,43 @@
+// Disjoint Set Union / Unoin Find (DSU)
+// Sky's the limit :)
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+const int N = 1e5 + 5;
+
+int par[N], sz[N];
+
+void make(int v) {
+    par[v] = v;
+    sz[v] = 1;
+}
+
+int find(int v) {
+    if (v == par[v])
+        return v;
+    return par[v] = find(par[v]);
+}
+
+void unions(int u, int v) {
+    u = find(u);
+    v = find(v);
+    if (u != v) {
+        if (sz[u] < sz[v])
+            swap(u, v);
+        par[v] = u;
+        sz[u] += sz[v];
+    }
+}
+
+signed main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+    int t = 1; 
+    // cin >> t; 
+    while (t--) {
+        
+    }
+    
+    return 0;
+}
