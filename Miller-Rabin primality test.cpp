1617a3de636083d293f648be3ca06b10f2678f31
marksman2op <56642081+marksman2op@users.noreply.github.com>
2020-07-13 19:25:26 +0530

    Update Miller-Rabin primality test.cpp

--- Miller-Rabin primality test.cpp
+++ Miller-Rabin primality test.cpp
@@ -30,18 +30,13 @@ bool check_composite(u64 n, u64 a, u64 d, int s) {
     }
     return true;
 }
- 
-bool MillerRabin(u64 n, int iter = 5) { // Probabilistic method 
-    if (n < 4)
+
+bool MillerRabin(u64 n, int iter = 5) {  // Probabilistic in O(k * (log n) ^ 3)
+    if (n < 2 || n % 6 % 4 != 1)             // Can be improved to O(k * (log n) ^ 2) by using FFT-based multiplication  
         return (n == 2 || n == 3);
  
-    int s = 0;
-    u64 d = n - 1;
-    while ((d & 1) == 0) {
-        d >>= 1;
-        s++;
-    }
- 
+    int s = __builtin_ctzll(n - 1);
+    u64 d = n >> s;
     for (int i = 0; i < iter; i++) {
         int a = 2 + rand() % (n - 3);
         if (check_composite(n, a, d, s))
