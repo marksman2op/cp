b530cfde77f49f5526ccacb010f00826a7b8d218
marksman2op <56642081+marksman2op@users.noreply.github.com>
2020-07-24 00:02:14 +0530

    Update Python basic.py

--- Python basic.py
+++ Python basic.py
@@ -30,7 +30,7 @@ def divide(a, b, n):
 def FastModularExponentiation(x, y, m):
     return pow(x, y, m)
 print(FastModularExponentiation(34, 60, 77))
-        
+
 def ModuloInverse(a, m):
     x0, x1, m0 = 0, 1, m
 
@@ -49,7 +49,6 @@ def ModuloInverse(a, m):
     Chinese Remainder Theorem:
     - If GCD(a, b) = 1, then there exists a number n such that n % a = ra and n % b = rb, for any pair (ra, rb).
     - If there are two such integers n1 and n2, n1 ≡ n2 (mod a * b).
-
     Proof:
     - GCD(a, b) = 1, so a * x + b * y = 1 for some integer (x, y).
     - ax ≡ 1 (mod b), by ≡ 1 (mod a)
@@ -63,3 +62,23 @@ def ChineseRemainderTheorem(n1, r1, n2, r2):
     # print(res % n1, res % n2)
     return res % n
 # print(ChineseRemainderTheorem(686579304, 295310485, 26855093, 8217207))
+
+"""
+    RSA Encryption:
+    - Public key E: Used to encrypt code, Available for everyone 
+    - Private key D: Used to decrypt code, Only available to Bob
+
+    Algorithm
+    - Generate 2 big random primes: p and q 
+    - To avoid attacks: 2048 bits long, |p - q| should be large, use proper seed to avoid insufficient randomness, add random padding to m before encryption
+    - n = p * q
+    - Generate random number e coprime with (p - 1) * (q - 1)
+    - E = (n, e)
+    - D = (p, q)
+
+    Encryption
+    - Convert message to sequence of bits
+    - Length in bits should be between 0 and (n - 1). (p, q, should be sufficiently large)
+    - Ciphertext c = pow(m, e) % n
+    - Decryption: Precompute d such that ed ≡ 1 mod (p - 1) * (q - 1) (Using Extended Euclid's Algorithm) (e'th Mdoular root of c)
+"""
