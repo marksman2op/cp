857f513aca716981c4aae768111e1dc54dc8dd40
marksman2op <56642081+marksman2op@users.noreply.github.com>
2020-07-27 23:44:37 +0530

    Update Closest pair of points.cpp

--- Closest pair of points.cpp
+++ Closest pair of points.cpp
@@ -70,9 +70,6 @@ void rec(int l, int r) {
 signed main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-
     int T = 1; 
     // cin >> T; 
     while (T--) {
@@ -93,6 +90,7 @@ signed main() {
         rec(0, n);
 
         int f = best_pair.first, s = best_pair.second;
+        cout << fixed << setprecision(8) << mindist << '\n';
     }
     
     return 0;
