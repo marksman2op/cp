0b0dbc1f54ba676b5b161a44455bc81c0ba97030
marksman2op <56642081+marksman2op@users.noreply.github.com>
2020-08-02 20:03:39 +0530

    Update Shank's SQUFOF.cpp

--- Shank's SQUFOF.cpp
+++ Shank's SQUFOF.cpp
@@ -334,7 +334,7 @@ unordered_map<int, int> mp;
 
 signed main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int t; 
+    int t = 1; 
     // cin >> t; 
     while(t--) {
         int n;
