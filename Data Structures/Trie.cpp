7bf482b1b9acb44783569fbded2454cedec01aa9
marksman2op <anilkumar12012016@gmail.com>
2020-08-18 23:02:39 +0530

    Trie

--- Data Structures/Trie.cpp
+++ Data Structures/Trie.cpp
@@ -1,57 +1,114 @@
-/*
-https://www.quora.com/q/threadsiiithyderabad/Tutorial-on-Trie-and-example-problems
-https://codeforces.com/blog/entry/75302
-https://codeforces.com/contest/456/problem/D
-*/
-
+// Sky's the limit :)
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long
 
-#define end _end
-#define next _nxt
+/*  
+    Trie 
+    - https://codeforces.com/blog/entry/43611
+    - LCP (Longest Common Prefix) of two strings is the LCA (Lowest Common Ancestor) of their nodes.
+    - Time Complexity: O(|key| * log(k))
+    - Space complexity: O(Σ |strings|)
+*/
 
-const int MaxN = 500500;
-int sz = 0;
+// https://codeforces.com/problemset/problem/455/B
 
-int next[27][MaxN];
-int end[MaxN];
-bool created[MaxN];
+struct Node {
+    bool isEnd;
+    Node *parent;
+    map<char, Node *> child;
+    Node(Node *parent = NULL)
+        : parent(parent), isEnd(false), child()
+    {}
+};
+ 
+Node *trie_search(Node *root, string str) {
+    Node *it;
+    int i = 0, n = str.size();
+    for (it = root; i < n; i++) {
+        if (it->child.find(str[i]) == it->child.end())
+            return NULL;
+        it = it->child[str[i]];
+    }
 
-void insert (string &s) {
-    int v = 0;
+    return (it->isEnd) ? it : NULL;
+}
+ 
+void trie_insert(Node *root, string str) {
+    Node *it;
+    int i = 0, n = str.size();
+    for (it = root; i < n; i++) {
+        if (it->child.find(str[i]) == it->child.end())
+            it->child[str[i]] = new Node(it);
+        it = it->child[str[i]];
+    }
+    it->isEnd = true;
+}
+ 
+void trie_erase(Node *root, string str) {
+    Node *it;
+    int i = 0, n = str.size();
+    for (it = root; i < n; i++) {
+        if (it->child.find(str[i]) == it->child.end())
+            return;
+        it = it->child[str[i]];
+    }
+    it->isEnd = false;
 
-    for (int i = 0; i < s.size(); ++i) {
-        int c = s[i] - 'a';
-        if (!created[next[c][v]]) {
-            next[c][v] = ++sz;
-            created[sz] = true;
+    while (it->parent != NULL) {
+        if (it->child.size() > 0 || it->isEnd)
+            break;
+        it = it->parent, i--;
+        it->child.erase(str[i]);
+    }
+}
+
+int dfs(Node *root) {
+    int res = 0;
+    bool got = false;
+    for (char c = 'a'; c <= 'z'; c++) {
+        if (root->child.find(c) != root->child.end()) {
+            got = true;
+            res |= dfs(root->child[c]) ^ 3;
         }
-        v = next[c][v];
     }
-    ++end[v];
+    if(!got)
+        res = 2;
+
+    return res;
 }
 
-bool search (string tmp) {
-    int v = 0;
+void print(int n) {
+    if (n == 1)
+        cout << "First\n";
+    else
+        cout << "Second\n";
+}
+
+signed main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     
-    for (int i = 0; i < tmp.size(); ++i) {
-        int c = tmp[i] - 'a';
-        if (!created[next[c][v]])
-            return false;
-        v = next[c][v];
+    int T = 1; 
+    // cin >> T; 
+    while (T--) {
+        int n, k;
+        cin >> n >> k;
+        Node *root = new Node();
+        for (int i = 0; i < n; i++) {
+            string s;
+            cin >> s;
+            trie_insert(root, s);
+        }
+
+        int ok = dfs(root);
+        if (ok == 0 || ok == 2)
+            print(2);
+        else if (ok == 1)
+            print(k % 2);
+        else
+            print(1);
     }
-    return end[v] > 0;
-}
-
-int main () {
-    string keys[] = {"hi", "hello", "you", "ekta", "me"};
-    string output[] = {"NO", "YES"};
-
-    for (int i = 0; i < 5; ++i)
-        insert (keys[i]);
-
-    cout << output[search ("my")] << endl;
-    cout << output[search ("me")] << endl;
     
     return 0;
 }
+
