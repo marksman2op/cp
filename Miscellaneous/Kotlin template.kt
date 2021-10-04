bda1bf982f4fbc8498579d4d9b30b43ef0f80b5d
Anil Kumar <56642081+marksman2op@users.noreply.github.com>
2021-10-04 13:29:39 +0530

    Update Kotlin template.kt

--- Miscellaneous/Kotlin template.kt
+++ Miscellaneous/Kotlin template.kt
@@ -57,17 +57,25 @@ fun main(args: Array<String>) {
 }
 
 /*
+@JvmField val _reader = System.`in`.bufferedReader()
+fun readLine(): String? = _reader.readLine()
+fun readLn() = _reader.readLine()!!
+@JvmField var _tokenizer: StringTokenizer = StringTokenizer("")
+fun read(): String {
+    while (_tokenizer.hasMoreTokens().not()) _tokenizer = StringTokenizer(_reader.readLine() ?: return "", " ")
+    return _tokenizer.nextToken()
+}
 fun readInt() = read().toInt()
 fun readDouble() = read().toDouble()
 fun readLong() = read().toLong()
 fun readStrings(n: Int) = List(n) { read() }
-fun readLines(n: Int) = List(n) { readLn() }
 fun readInts(n: Int) = List(n) { read().toInt() }
-fun readIntArray(n: Int) = IntArray(n) { read().toInt() }
 fun readDoubles(n: Int) = List(n) { read().toDouble() }
-fun readDoubleArray(n: Int) = DoubleArray(n) { read().toDouble() }
 fun readLongs(n: Int) = List(n) { read().toLong() }
-fun readLongArray(n: Int) = LongArray(n) { read().toLong() }
+ 
+@JvmField val _writer = PrintWriter(System.out, false)
+inline fun output(block: PrintWriter.()->Unit) { _writer.apply(block).flush() }
+fun iprintln(o: Any?) { println(o) } // immediate println for interactive, bypasses output{} blocks
 */
 
 /*
