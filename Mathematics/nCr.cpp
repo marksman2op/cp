87605e13504b41a5ca983bcb3269896555abaa47
Anil Kumar <56642081+marksman2op@users.noreply.github.com>
2024-11-24 02:07:26 +0530

    Update nCr.cpp

--- Mathematics/nCr.cpp
+++ Mathematics/nCr.cpp
@@ -1,21 +1,27 @@
-//nCr in O(n)
-const int MOD = 1e9 + 7;
-const int  N = 1e6 + 5;
 
-int fac[N], ifac[N];
+struct Combinatorics {
+    const int n, mod;
+    vector<int> fact, invFact;
 
-void pre() { 
-    fac[0] = ifac[0] = ifac[1] = 1;
-    for (int i = 1; i < N; i++) 
-        fac[i] = i * fac[i - 1] % MOD;
-    for (int i = 2; i < N; i++) 
-        ifac[i] = ifac[MOD % i] * (MOD - MOD / i) % MOD;
-    for (int i = 1; i < N; i++) 
-        ifac[i]= ifac[i] * ifac[i - 1] % MOD;
-}
+    Combinatorics(int n, int mod) : n(n), mod(mod) {
+        fact.resize(n + 1);
+        invFact.resize(n + 1);
+        fact[0] = invFact[0] = invFact[1] = 1;
+        for (int i = 1; i <= n; i++) {
+            fact[i] = (long)i * fact[i - 1] % mod;
+        }
+        for (int i = 2; i <= n; i++) {
+            invFact[i] = (long)invFact[mod % i] * (mod - mod / i) % mod;
+        }
+        for (int i = 2; i <= n; i++) {
+            invFact[i] = (long)invFact[i] * invFact[i - 1] % mod;
+        }
+    }
 
-int choose(int n, int r) {
-    if (r < 0 || r > n)
-        return 0;
-    return fac[n] * ifac[r] % MOD * ifac[n - r] % MOD;
-}
+    int choose(int n, int r) {
+        if (r < 0 || r > n) {
+            return 0;
+        }
+        return (long)fact[n] * invFact[r] % mod * invFact[n - r] % mod;
+    }
+};
